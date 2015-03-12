45e5bf567275f82ff25d72b91fa754ce101aaf3b
Tom Dean <tomddean@theinternetisawesome.com>
2015-03-12 16:05:49 -0400

    Extracted event data into its own server and added a gravatar image\!

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -1,49 +1,20 @@
 'use strict';
 
-eventsApp.controller('EventController', function EventController($scope) {
+eventsApp.controller('EventController', function EventController($scope, eventData) {
     $scope.sortorder = 'name';
-    $scope.event = {
-        name: 'Angular Boot Camp',
-        date: '1/1/2013',
-        time: '10:30 am',
-        location: {
-            address: "Google Headquarters",
-            city: "Mountain View",
-            province: "CA"
-        },
-        sessions: [
-            {
-                name: 'Directives Masterclass',
-                creatorName: 'Bob Smith',
-                duration: '1 hour',
-                level: 'Advanced',
-                abstract: 'In this session you will learn stuff...',
-                upVoteCount:0
-            },
-            {
-                name: 'Scopes for fun and profit',
-                creatorName: 'John Doe',
-                duration: '30 mins',
-                level: 'Introductory',
-                abstract: 'This session will take a closer look at scopes.',
-                upVoteCount:0
-            },
-            {
-                name: 'Well Behaved Controllers',
-                creatorName: 'Jane Doe',
-                duration: '2 hours',
-                level: 'Intermediate',
-                abstract: 'Controllers are the beginning of everything Angular does!',
-                upVoteCount:0
-            }
-        ]
-    }
+    $scope.event = eventData.event;
 
-    $scope.upVoteSession = function(session) {
+    $scope.upVoteSession = function (session) {
         session.upVoteCount++;
     }
 
-    $scope.downVoteSession = function(session) {
+    $scope.downVoteSession = function (session) {
         session.upVoteCount--;
+    }
+
+    $scope.getGravatarImage = function (email) {
+        if (email !== undefined) {
+            return 'https://gravatar.com/avatar/' + md5(email) + '.jpg?s=200&r=g';
+        }
     }
 });
---
+++ DemoApp/app/js/services/EventData.js
@@ -0,0 +1,41 @@
+eventsApp.factory('eventData', function () {
+    return {
+        event: {
+            name: 'Angular Boot Camp',
+            date: '1/1/2013',
+            time: '10:30 am',
+            location: {
+                address: "Google Headquarters",
+                city: "Mountain View",
+                province: "CA"
+            },
+            sessions: [
+                {
+                    name: 'Directives Masterclass',
+                    creatorName: 'Bob Smith',
+                    duration: '1 hour',
+                    level: 'Advanced',
+                    abstract: 'In this session you will learn stuff...',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Scopes for fun and profit',
+                    creatorName: 'John Doe',
+                    duration: '30 mins',
+                    level: 'Introductory',
+                    abstract: 'This session will take a closer look at scopes.',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Well Behaved Controllers',
+                    creatorName: 'Jane Doe',
+                    duration: '2 hours',
+                    level: 'Intermediate',
+                    abstract: 'Controllers are the beginning of everything Angular does!',
+                    upVoteCount: 0
+                }
+            ]
+        }
+    };
+});
+
